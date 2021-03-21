6004d7354f7cb7f1fbec6875cadab87b3ba43d84
DeboleenaSe <deboleena1996@gmail.com>
2021-03-21 12:30:31 +0530

    angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sumit'`, () => {
+  it(`should have as title 'deboleena'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sumit');
+    expect(app.title).toEqual('deboleena');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sumit app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('deboleena app is running!');
   });
 });
